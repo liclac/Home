1a7aec8358aa39c831816df811348122d9c38842
Johannes Ekberg <uppfinnarn@gmail.com>
2013-04-25 00:30:58 +0200

    100/100 on Google PageSpeed

--- home/settings.py
+++ home/settings.py
@@ -122,6 +122,7 @@ MIDDLEWARE_CLASSES = (
 	'django.middleware.common.CommonMiddleware',
 	'django.contrib.sessions.middleware.SessionMiddleware',
 	'django.middleware.csrf.CsrfViewMiddleware',
+	'django.middleware.gzip.GZipMiddleware',
 	'django.contrib.auth.middleware.AuthenticationMiddleware',
 	'django.contrib.messages.middleware.MessageMiddleware',
 	# Uncomment the next line for simple clickjacking protection:
