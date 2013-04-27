811df3e973d62062626611d018ce67b366bdd9eb
Johannes Ekberg <uppfinnarn@gmail.com>
2013-04-27 03:49:16 +0200

    Cache!

--- home.py
+++ home.py
@@ -1,7 +1,9 @@
 import os
-from flask import Flask, render_template, abort
+from functools import wraps
+from flask import Flask, render_template, request, abort
 from fileblog import Post
 from middleware import PathFix
+from werkzeug.contrib.cache import MemcachedCache
 
 app = Flask(__name__)
 # I want this on /, even though mod_rewrite/mod_wsgi doesn't.
@@ -9,11 +11,31 @@ app = Flask(__name__)
 # says it should be.
 app.wsgi_app = PathFix(app.wsgi_app, '/')
 
+cache = MemcachedCache(['127.0.0.1:11211'])
+
 path = os.path.dirname(__file__)
 posts_path = os.path.join(path, 'posts')
 
 
 
+# Decorators
+def cached(timeout=60*60, key='view/%s'):
+	def decorator(f):
+		@wraps(f)
+		def decorated_function(*args, **kwargs):
+			cache_key = key % request.path
+			rv = cache.get(cache_key)
+			if rv is not None:
+				return rv
+			rv = f(*args, **kwargs)
+			cache.set(cache_key, rv, timeout=timeout)
+			return rv
+		return decorated_function
+	return decorator
+
+
+
+# Templates
 @app.context_processor
 def blog_processor():
 	return {
@@ -23,15 +45,19 @@ def blog_processor():
 
 
 
+# Routes
 @app.route('/')
+@cached()
 def home():
 	return render_template('home.html')
 
 @app.route('/blog/')
+@cached()
 def blog():
 	return render_template('blog.html')
 
 @app.route('/blog/<slug>/')
+@cached()
 def blog_post(slug):
 	try:
 		post = Post.slug(posts_path, slug)
