5cfed441e238b8e832ed5ec951edee29f557e638
Johannes Ekberg <uppfinnarn@gmail.com>
2013-04-27 11:32:20 +0200

    Fileblog now parses the first header rather than a metadata block

--- fileblog.py
+++ fileblog.py
@@ -1,9 +1,19 @@
 import os
+import re
 import markdown2
 from datetime import datetime
 from operator import attrgetter
 
 markdowner = markdown2.Markdown(extras=['metadata', 'fenced-code-blocks'])
+
+title_exp = re.compile(r'([^\r\n]+)\r?\n[=-]+(\r?\n)*')
+def extract_title(text):
+	match = title_exp.match(text)
+	if match:
+		text = text[match.end():]
+		title = match.group(1)
+		return (title, text)
+	return ('Untitled', text)
 
 class Post(object):
 	def __init__(self, path, full=True):
@@ -14,11 +24,13 @@ class Post(object):
 			text = f.read()
 			if not full:
 				text = text.split('\n\n')[0]
+			title, text = extract_title(text)
 			
 			self.html = markdowner.convert(text)
 			self.slug = '.'.join(os.path.basename(path).split(os.extsep)[:-1])
-			for key, value in self.html.metadata.iteritems():
-				setattr(self, key, value)
+			self.title = title
+			#for key, value in self.html.metadata.iteritems():
+			#	setattr(self, key, value)
 	
 	@classmethod
 	def list(cls, path):
